5764b03591ffaa4e4a694e31c09cf8b545bfab86
Jahongir <zokirov123404321@gmail.com>
2023-01-22 21:49:12 +0500

    Project created

---
+++ src/main/java/com/messenger/MessengerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerDemoApplication.class, args);
+    }
+
+}
